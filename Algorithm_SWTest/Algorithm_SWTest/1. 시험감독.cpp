6ae08771a98f178c246629b1512d4497a97d89da
dbsrlwo1@gmail.com <dbsrlwo1@gmail.com>
2020-04-08 17:59:34 +0900

    test

---
+++ Algorithm_SWTest/Algorithm_SWTest/1. 시험감독.cpp
@@ -0,0 +1,31 @@
+#pragma warning (disable :4996)
+#include <iostream>
+using namespace std;
+int N; // 시험장의 갯수
+int B, C; // 총감독체크 인원 , 부감독 체크인원
+int A[1000000];
+int main() {
+    //freopen("input.txt","r",stdin);
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++) {
+        int input;
+        scanf("%d", &A[i]);
+    }
+    scanf("%d %d", &B, &C);
+    long long cnt = 0;
+    for (int i = 0; i < N; i++) {
+        if (A[i] - B <= 0) {
+            cnt++;
+        }
+        else {
+            if ((A[i] - B) % C != 0) {
+                cnt += (A[i] - B) / C + 2;
+            }
+            else {
+                cnt += (A[i] - B) / C + 1;
+            }
+        }
+    }
+    cout << cnt;
+    return 0;
+}
